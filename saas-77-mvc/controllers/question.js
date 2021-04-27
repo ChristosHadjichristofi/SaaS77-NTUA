73afe8fa5355ea465100257b2af66c819073198f
Christos Hadjichristofi <40044042+BeenCoding@users.noreply.github.com>
2021-04-28 01:10:42 +0300

    Finish ask question view and logic

---
+++ saas-77-mvc/controllers/question.js
@@ -0,0 +1,36 @@
+// require models
+const sequelize = require('../utils/database');
+var initModels = require("../models/init-models");
+var models = initModels(sequelize);
+// end of require models
+
+exports.createQuestion = (req, res, next) => {
+
+    let qname = req.body.qname;
+    let qtext = req.body.qtext;
+    let qkeywords = req.body.qkeywords;
+
+    const keywordsArr = qkeywords.split(',');
+    console.log(keywordsArr)
+    models.Questions.create({
+        title: qname,
+        text: qtext,
+        dateCreated: Date.now(),
+        UsersId: req.session.userID
+    })
+    .then(question => {
+        
+        let insertKeywords = new Promise((resolve, reject) => {
+            keywordsArr.forEach((keywordName, index) => {
+                models.Keywords.create({
+                    name: keywordName,
+                    QuestionsId: question.id
+                })
+                if (index === keywordsArr.length -1) return resolve(true);
+            });
+        });
+        
+        insertKeywords.then(() => { res.redirect('/home'); });
+    })
+
+}
--- saas-77-mvc/routes/question.js
+++ saas-77-mvc/routes/question.js
@@ -4,7 +4,7 @@ const questionController = require('../controllers/question');
 
 const router = express.Router();
 
-// router.post('/create', questionController.createQuestion);
+router.post('/create', questionController.createQuestion);
 
 // router.post('/answer', questionController.answerQuestion);
 
