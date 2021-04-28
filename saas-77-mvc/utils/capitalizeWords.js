37f3e2825f9fecdd1a69352675f1ef57de34edc9
Christos Hadjichristofi <40044042+BeenCoding@users.noreply.github.com>
2021-04-28 14:12:49 +0300

    Add utility to capitalize a word

---
+++ saas-77-mvc/utils/capitalizeWords.js
@@ -0,0 +1,7 @@
+ module.exports = (word) => {
+
+    const wordLowerCase = word.toLowerCase();
+    const wordCapitalized = wordLowerCase[0].toUpperCase() + wordLowerCase.substring(1);
+
+    return wordCapitalized;
+}
