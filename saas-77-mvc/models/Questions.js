48aeddbaf606392cc1ed711189d49b1897832303
panoszvg <44406295+panoszvg@users.noreply.github.com>
2021-05-01 00:50:58 +0300

    Fix bug on Questions model

--- saas-77-mvc/models/Questions.js
+++ saas-77-mvc/models/Questions.js
@@ -1,45 +1,43 @@
 const Sequelize = require('sequelize');
 module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('Questions', {
-    id: {
-      autoIncrement: true,
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true
-    },
-    title: {
-      type: DataTypes.STRING(255),
-      allowNull: false
-    },
-    text: {
-      type: DataTypes.TEXT,
-      allowNull: false
-    },
-    dateCreated: {
-      type: DataTypes.DATE,
-      allowNull: false
-    },
-    UsersId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'Questions',
-        key: 'id'
-      }
-    }
-  }, {
-    sequelize,
-    tableName: 'Questions',
-    schema: 'saas-77-mvc',
-    timestamps: false,
-    indexes: [
-      {
-        name: "Questions_pkey",
-        unique: true,
-        fields: [
-          { name: "id" },
-        ]
-      },
-    ]
-  });
+    return sequelize.define('Questions', {
+        id: {
+            autoIncrement: true,
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true
+        },
+        title: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        text: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        dateCreated: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        UsersId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'Users',
+                key: 'id'
+            }
+        }
+    }, {
+        sequelize,
+        tableName: 'Questions',
+        schema: 'saas-77-mvc',
+        timestamps: false,
+        indexes: [{
+            name: "Questions_pkey",
+            unique: true,
+            fields: [
+                { name: "id" },
+            ]
+        }, ]
+    });
 };
