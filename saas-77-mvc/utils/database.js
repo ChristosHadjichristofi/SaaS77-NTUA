4c9da8255e6bffa0b73dd9cbd31af18b3d5baaf6
Christos Hadjichristofi <40044042+BeenCoding@users.noreply.github.com>
2021-04-30 14:34:21 +0300

    Add timezone to database

--- saas-77-mvc/utils/database.js
+++ saas-77-mvc/utils/database.js
@@ -4,7 +4,8 @@ require('custom-env').env('localhost')
 sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASS, {
     dialect: 'postgres',
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    timezone: process.env.DB_TIMEZONE
 });
 
 sequelize.authenticate()
