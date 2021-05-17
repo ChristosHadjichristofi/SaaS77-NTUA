37759db8dc2136e67ccec76c39baa90452ceb788
Christos Hadjichristofi <40044042+BeenCoding@users.noreply.github.com>
2021-05-18 01:00:47 +0300

    Remove signoutcontroller require

--- saas-77-microservices/accounts-service/routes/account.js
+++ saas-77-microservices/accounts-service/routes/account.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 const signInController = require('../controllers/signIn');
 const signUpController = require('../controllers/signUp');
-const signOutController = require('../controllers/signOut');
 const signupValidator = require('../validation/signupValidator');
 const router = express.Router();
 
