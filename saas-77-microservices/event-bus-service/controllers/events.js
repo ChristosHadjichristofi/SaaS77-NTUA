ddf276b184a69cba4e50b21a9f36d4d56702e44d
Christos Hadjichristofi <40044042+BeenCoding@users.noreply.github.com>
2021-05-18 15:59:52 +0300

    Update event bus to make axios requests on /events routes

--- saas-77-microservices/event-bus-service/controllers/events.js
+++ saas-77-microservices/event-bus-service/controllers/events.js
@@ -1,11 +1,57 @@
+const axios = require('axios');
+
 module.exports = (req, res, next) => {
     
-    console.log(req.headers.host)
-
     const event = req.body;
+    
+    const url_answersService = 'http://localhost:4002/events';
+    const url_browseQuestionsService = 'http://localhost:4003/events';
+    const url_analyticsService = 'http://localhost:4004/events';
+    const url_graphsService = 'http://localhost:4005/events';
 
-    console.log(event);
+    const data = event;
+    let responses = [];
 
-    return res.status(200).json({ message: 'OK.' })
+    const config_answersService = { method: 'post', url: url_answersService, data: data };
+    const config_browseQuestionsService = { method: 'post', url: url_browseQuestionsService, data: data };
+    const config_analyticsService = { method: 'post', url: url_analyticsService, data: data };
+    const config_graphsService = { method: 'post', url: url_graphsService, data: data };
 
+    let answersServicePromise = new Promise((resolve, reject) => { 
+
+        axios(config_answersService)
+        .then(result => { responses.push({ status: 200, message: 'OK - ANSWERS SERVICE.' } ); return resolve(); })
+        .catch(err => { responses.push({ status: 500, message: 'NOT OK - ANSWERS SERVICE.' } ); return resolve(); });
+    
+    })
+    
+    let browseQuestionsServicePromise = new Promise((resolve, reject) => { 
+
+        axios(config_browseQuestionsService)
+        .then(result => { responses.push({ status: 200, message: 'OK - BROWSE QUESTIONS SERVICE.' } ); return resolve(); })
+        .catch(err => { responses.push({ status: 500, message: 'NOT OK - BROWSE QUESTIONS SERVICE.' } ); return resolve(); });
+    })
+
+    let analyticsServicePromise = new Promise((resolve, reject) => { 
+        axios(config_analyticsService)
+        .then(result => { responses.push({ status: 200, message: 'OK - ANALYTICS SERVICE.' } ); return resolve(); })
+        .catch(err => { responses.push({ status: 500, message: 'NOT OK - ANALYTICS SERVICE.' } ); return resolve(); });
+    })
+
+    let graphsServicePromise = new Promise((resolve, reject) => { 
+        
+        axios(config_graphsService)
+        .then(result => { responses.push({ status: 200, message: 'OK - GRAPHS SERVICE.' } ); return resolve(); })
+        .catch(err => { responses.push({ status: 500, message: 'NOT OK - GRAPHS SERVICE.' } ); return resolve(); });
+    })
+
+    Promise.all([answersServicePromise, browseQuestionsServicePromise, analyticsServicePromise, graphsServicePromise]).then(() => {
+        
+        let isOK = true;
+
+        responses.forEach(el => {
+            isOK = (el.status == 200 && isOK) ? true : false;
+        });
+        res.status(200).json({ message: (isOK) ? 'OK' : 'NOT OK' })
+    })
 }
