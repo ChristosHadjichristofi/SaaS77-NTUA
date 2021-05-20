3be9cfc614872fd652871b58f280339e1f492a57
Christos Hadjichristofi <40044042+BeenCoding@users.noreply.github.com>
2021-05-20 11:28:18 +0300

    Fix bug on analytics service

--- saas-77-microservices/analytics-service/controllers/analytics.js
+++ saas-77-microservices/analytics-service/controllers/analytics.js
@@ -17,8 +17,8 @@ exports.stats = (req, res, next) => {
     let analyticsPromise = new Promise((resolve, reject) => {
         models.Users.findAll({ raw: true, where: { id: userData.user.id } })
             .then(row => {
-                totalQuestions = row[0].questions;
-                totalAnswers = row[0].answers;
+                totalQuestions = row[0].questions.length;
+                totalAnswers = row[0].answers.length;
                 resolve();
             })
     })
