917faba894d729a9a3e46ff86c5da09dc0ac1da8
Christos Hadjichristofi <40044042+BeenCoding@users.noreply.github.com>
2021-05-20 12:34:42 +0300

    Chance request type to /question/:id

--- saas-77-microservices/answers-service/routes/answers.js
+++ saas-77-microservices/answers-service/routes/answers.js
@@ -5,7 +5,7 @@ const isAuth = require('../middlewares/authentication')
 
 const router = express.Router();
 
-router.get('/question/:id', isAuth, answersController.getQuestion);
+router.post('/question/:id', isAuth, answersController.getQuestion);
 
 router.post('/answer/:id', isAuth, answersController.postAnswer);
 
