fdccc461ca3eea20e928bf566aab1334b3e75a95
Christos Hadjichristofi <40044042+BeenCoding@users.noreply.github.com>
2021-05-28 16:38:24 +0300

    Return to localhost env

--- saas-77-mvc/utils/database.js
+++ saas-77-mvc/utils/database.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize')
-require('custom-env').env('production')
+require('custom-env').env('localhost')
 
 sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASS, {
     dialect: 'postgres',
