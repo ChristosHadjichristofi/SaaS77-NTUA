005a63bf9d9d7a2899238e228c4c97470ad9bb28
Christos Hadjichristofi <40044042+BeenCoding@users.noreply.github.com>
2021-06-01 14:53:21 +0300

    Add clear console functionality to CLI

--- saas-77-microservices/event-bus-service/utils/cli.js
+++ saas-77-microservices/event-bus-service/utils/cli.js
@@ -1,4 +1,5 @@
 const modController = require('../controllers/moderate');
+const clear = require('clear');
 const chalk = require('chalk');
 
 process.stdin.resume();
@@ -7,7 +8,9 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', (text) => {
     if (text.trim() === 'commands --list') 
         console.log(`Commands List: 
-        1. Subscribers --status`)
+        1. Subscribers --status
+        2. clear`)
     else if (text.trim() === 'Subscribers --status') modController.moderate();
+    else if (text.trim() === 'clear') clear();
     else console.log(chalk.red(`Unknown CLI command. Use commands --list to see the available commands.`));
 });
