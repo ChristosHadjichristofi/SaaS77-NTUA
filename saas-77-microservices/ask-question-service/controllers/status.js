4721dfc33f8494b6824f9e5d3118af7a0ec31eed
Christos Hadjichristofi <40044042+BeenCoding@users.noreply.github.com>
2021-06-02 19:13:20 +0300

    Update status.js

--- saas-77-microservices/ask-question-service/controllers/status.js
+++ saas-77-microservices/ask-question-service/controllers/status.js
@@ -1,11 +1,3 @@
-// require models
-const sequelize = require('../utils/database');
-// end of require models
-
 module.exports = (req, res, next) => {
-
-    sequelize.authenticate()
-    .then(() => res.status(200).json({ service: 'Create Question', status: 'UP', uptime: Math.floor(process.uptime()), database: 'Connection - OK' }))
-    .catch(err => res.status(200).json({ service: 'Create Question', status: 'UP', uptime: Math.floor(process.uptime()), database: 'Connection - FAILED' }))
-
+    return res.status(200).json({ service: 'Create Question', status: 'UP', uptime: Math.floor(process.uptime()), database: 'No Database' });
 }
