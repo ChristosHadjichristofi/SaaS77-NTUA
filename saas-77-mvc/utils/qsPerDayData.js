1ebe2a4cd6c6b887ed78d90d9c431c08808f7ea3
panoszvg <44406295+panoszvg@users.noreply.github.com>
2021-06-06 14:16:28 +0300

    Add moderation screenshots to Documentation Page & Make graph contain 5 days

--- saas-77-mvc/utils/qsPerDayData.js
+++ saas-77-mvc/utils/qsPerDayData.js
@@ -11,13 +11,13 @@ module.exports = () => {
     return new Promise((resolve, reject) => {
         sequelize.query('SELECT DATE("dateCreated") AS "date", COUNT(*) AS "count" '
         + 'FROM "saas-77-mvc"."Questions" AS "Questions" '
-        + 'GROUP BY "date" ORDER BY "date" DESC LIMIT 21 ', { type: sequelize.QueryTypes.SELECT })
+        + 'GROUP BY "date" ORDER BY "date" DESC LIMIT 17 ', { type: sequelize.QueryTypes.SELECT })
         .then(result => {
 
             let index = 0;
             const now = new Date(Date.now());
             const endDate = new Date();
-            endDate.setDate(now.getDate() - 21)
+            endDate.setDate(now.getDate() - 17)
             if (result.length !== 0) {
                 while (result[index].date.localeCompare(now.toISOString().split('T')[0]) == 1) index++
                 for (var d = now; d.toISOString().split('T')[0] !== endDate.toISOString().split('T')[0]; d.setDate(d.getDate() - 1)) {
