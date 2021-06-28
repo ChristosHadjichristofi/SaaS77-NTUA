7363743658edde5a7301081f215a5282a6b17379
panoszvg <44406295+panoszvg@users.noreply.github.com>
2021-06-29 00:29:35 +0300

    Fix 'No Keywords' bug

--- saas-77-microservices/ask-question-service/controllers/createQuestion.js
+++ saas-77-microservices/ask-question-service/controllers/createQuestion.js
@@ -34,7 +34,7 @@ module.exports = (req, res, next) => {
     */
     let keywordsArr = qkeywords.split(',');
     const keywordsSet = new Set(keywordsArr);
-    keywordsArr = Array.from(keywordsSet);
+    keywordsArr = (keywordsSet.size == 1 && keywordsSet.has('')) ? [] : Array.from(keywordsSet);
     
     // create the question using the body
     const url = 'http://localhost:4006/events';
