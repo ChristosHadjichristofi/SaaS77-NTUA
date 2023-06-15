24186391df3af5556beaaf9cd255dc5d868ac51b
Christos Hadjichristofi <christoshadjichristofi@hotmail.com>
2023-06-15 12:27:01 +0300

    Add functionality create schema if not exists

--- saas-77-microservices/accounts-service/server.js
+++ saas-77-microservices/accounts-service/server.js
@@ -1,23 +1,52 @@
 const app = require("./app");
-
 const chalk = require("chalk");
 
 // for database
 const sequelize = require("./utils/database");
-var initModels = require("./models/init-models");
+const initModels = require("./models/init-models");
 
 const port = Number(4000);
 
-initModels(sequelize);
-sequelize
-    .sync({
-        // delete if system is ready to deploy
-        force: true,
-        // end
-    })
-    .then((result) => {
-        app.listen(port, () => {
-            console.log(chalk.green(`🚀 Accounts Service running on port ${port}!`));
-        });
-    })
-    .catch((err) => console.log(err));
+const createSchema = async () => {
+  try {
+    // Connect to the database
+    await sequelize.authenticate();
+
+    // Create the schema (database)
+    await sequelize.query(`CREATE SCHEMA IF NOT EXISTS ${ process.env.DB_SCHEMA }`);
+
+    console.log("Schema created successfully");
+  } catch (error) {
+    console.error("Error creating schema:", error);
+  } finally {
+    // Close the database connection
+    await sequelize.close();
+  }
+};
+
+const startServer = async () => {
+  try {
+    // Create the schema (database)
+    await createSchema();
+
+    // Initialize Sequelize models
+    initModels(sequelize);
+
+    // Sync models with the database
+    await sequelize.sync({
+      // delete if system is ready to deploy
+      force: true,
+      // end
+    });
+
+    // Start the server
+    app.listen(port, () => {
+      console.log(chalk.green(`🚀 Accounts Service running on port ${port}!`));
+    });
+  } catch (error) {
+    console.error("Error starting server:", error);
+  }
+};
+
+// Call startServer function to create schema and start the server
+startServer();
