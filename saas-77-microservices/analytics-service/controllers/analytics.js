885abff61110377177db063aa8c1af9bbdc72206
Christos Hadjichristofi <christoshadjichristofi@hotmail.com>
2023-06-15 13:06:48 +0300

    Revert "hope it works, if not :("
    This reverts commit b0d30776c4f1ee4325eca40aab34293b83b42577.

--- saas-77-microservices/analytics-service/controllers/analytics.js
+++ saas-77-microservices/analytics-service/controllers/analytics.js
@@ -1,126 +1,96 @@
 const jwt_decode = require('jwt-decode');
 
 // require models
-const createSequelizeInstance = require('../utils/database');
+const sequelize = require('../utils/database');
+var initModels = require("../models/init-models");
+var models = initModels(sequelize);
+// end of require models
 
 const calcDays = require('../utils/calcDays');
 
-exports.stats = async (req, res, next) => {
-  try {
+exports.stats = (req, res, next) => {
+
     let totalQuestions, totalAnswers, daysRegistered, contributions;
 
     /* retrieve user data from AUTH Header */
     const userData = jwt_decode(req.header('X-OBSERVATORY-AUTH'));
 
-    // Create the Sequelize instance
-    const sequelize = await createSequelizeInstance();
-
-    // Initialize Sequelize models
-    const initModels = require("../models/init-models");
-    const models = initModels(sequelize);
-
     /* retrieve all questions and answers that user with id userData.user.id submitted */
     let analyticsPromise = new Promise((resolve, reject) => {
-      models.Users.findAll({ raw: true, where: { id: userData.user.id } })
+        models.Users.findAll({ raw: true, where: { id: userData.user.id } })
         .then(row => {
-          totalQuestions = row[0].questions;
-          totalAnswers = row[0].answers;
-          resolve();
+            totalQuestions = row[0].questions;
+            totalAnswers = row[0].answers;
+            resolve();
         })
-        .catch(err => {
-          return res.status(500).json({ message: 'Internal server error', type: 'error' });
-        });
-    });
+        .catch(err => { return res.status(500).json({ message: 'Internal server error', type: 'error' }); })
+    })
 
     /* after retrieving all data from analyticsPromise calculate days registered and contributions */
     Promise.all([analyticsPromise]).then(() => {
+        
+        daysRegistered = calcDays(Date.now(), new Date(userData.user.dateCreated));
+        
+        contributions = totalAnswers / daysRegistered;
 
-      daysRegistered = calcDays(Date.now(), new Date(userData.user.dateCreated));
-
-      contributions = totalAnswers / daysRegistered;
-
-      /* return response of status OK and json object containing all necessary info the frontend needs */
-      return res.status(200).json({
-        totalQuestions: totalQuestions,
-        totalAnswers: totalAnswers,
-        daysRegistered: daysRegistered,
-        contributions: contributions.toFixed(2),
-      });
+        /* return response of status OK and json object containing all necessary info the frontend needs */
+        return res.status(200).json({
+            totalQuestions: totalQuestions,
+            totalAnswers: totalAnswers,
+            daysRegistered: daysRegistered,
+            contributions: contributions.toFixed(2),
+        })
 
     })
-      .catch(err => {
-        return res.status(500).json({ message: 'Internal server error', type: 'error' });
-      });
+    .catch(err => { return res.status(500).json({ message: 'Internal server error', type: 'error' }); })
 
-  } catch (error) {
-    console.error("Error retrieving stats:", error);
-    return res.status(500).json({ message: 'Internal server error', type: 'error' });
-  }
-};
+}
 
 /* events route (subscriber on bus events - QUESTION CREATE/ ANSWER CREATE/ USER CREATE) */
-exports.events = async (req, res, next) => {
-  try {
+exports.events = (req, res, next) => {
+
     const type = req.body.type;
-
-    // Create the Sequelize instance
-    const sequelize = await createSequelizeInstance();
-
-    // Initialize Sequelize models
-    const initModels = require("../models/init-models");
-    const models = initModels(sequelize);
 
     /* increment the events counter (how many events this service processed) */
     models.Events.increment('counter', { by: 1, where: { id: 1 } })
-      .then(() => {
-
+    .then(() => {
+        
         /* Based on which Type of event is the service has a specific behaviour */
         if (type === 'QUESTION CREATE') {
-
-          models.Users.increment('questions', { by: 1, where: { id: req.body.usersId } })
+    
+            models.Users.increment('questions', { by: 1, where: { id: req.body.usersId } })
             .then(() => res.status(200).json({}))
-            .catch(() => res.status(500).json({ message: 'Internal server error', type: 'error' }));
-
+            .catch(() => res.status(500).json({ message: 'Internal server error', type: 'error' }))
+    
         } else if (type === 'ANSWER CREATE') {
-
-          models.Users.increment('answers', { by: 1, where: { id: req.body.usersId } })
+    
+            models.Users.increment('answers', { by: 1, where: { id: req.body.usersId } })
             .then(() => res.status(200).json({}))
-            .catch(() => res.status(500).json({ message: 'Internal server error', type: 'error' }));
-
+            .catch(() => res.status(500).json({ message: 'Internal server error', type: 'error' }))
+    
         } else if (type === 'USER CREATE') {
-
-          models.Users.create({ id: req.body.usersId, questions: 0, answers: 0 })
+    
+            models.Users.create({ id: req.body.usersId, questions: 0, answers: 0 })
             .then(() => res.status(200).json({}))
-            .catch(() => res.status(500).json({ message: 'Internal server error', type: 'error' }));
-
+            .catch(() => res.status(500).json({ message: 'Internal server error', type: 'error' }))
+    
         } else {
-          res.status(200).json({});
+            res.status(200).json({});
         }
+    
+    })
+    .catch(() => res.status(500).json({ message: 'Internal server error', type: 'error' }))
 
-      })
-      .catch(() => res.status(500).json({ message: 'Internal server error', type: 'error' }));
-
-  } catch (error) {
-    console.error("Error processing events:", error);
-    return res.status(500).json({ message: 'Internal server error', type: 'error' });
-  }
-};
+}
 
 /** function that returns the status of this specific service 
  * tries sequelize.authenticate. If successful then connection to database is OK
  * else its not OK
  */
-exports.status = async (req, res, next) => {
-  try {
-    // Create the Sequelize instance
-    const sequelize = await createSequelizeInstance();
+exports.status = (req, res, next) => {
 
     sequelize.authenticate()
-      .then(() => res.status(200).json({ service: 'Analytics', status: 'UP', uptime: Math.floor(process.uptime()), database: 'Connection - OK' }))
-      .catch(err => res.status(200).json({ service: 'Analytics', status: 'UP', uptime: Math.floor(process.uptime()), database: 'Connection - FAILED' }));
+    .then(() => res.status(200).json({ service: 'Analytics', status: 'UP', uptime: Math.floor(process.uptime()), database: 'Connection - OK' }))
+    .catch(err => res.status(200).json({ service: 'Analytics', status: 'UP', uptime: Math.floor(process.uptime()), database: 'Connection - FAILED' }))
 
-  } catch (error) {
-    console.error("Error retrieving service status:", error);
-    return res.status(500).json({ message: 'Internal server error', type: 'error' });
-  }
-};
+}
--- saas-77-microservices/analytics-service/utils/database.js
+++ saas-77-microservices/analytics-service/utils/database.js
@@ -1,26 +1,19 @@
-const Sequelize = require('sequelize');
-require('custom-env').env('localhost');
+const Sequelize = require('sequelize')
+require('custom-env').env('localhost')
 
-const createSequelizeInstance = () => {
-  return new Promise((resolve, reject) => {
-    const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASS, {
-      dialect: 'postgres',
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      timezone: process.env.DB_TIMEZONE
-    });
+sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASS, {
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    timezone: process.env.DB_TIMEZONE
+});
 
-    sequelize
-      .authenticate()
-      .then(() => {
-        console.log("Success connecting to the database!");
-        resolve(sequelize);
-      })
-      .catch(err => {
-        console.error("Unable to connect to the database", err);
-        reject(err);
-      });
-  });
-};
+sequelize.authenticate()
+.then(() => {
+    console.log("Success connecting to database!");    
+})
+.catch(err => {
+    console.error("Unable to connect to the database", err);
+})
 
-module.exports = createSequelizeInstance;
+module.exports = sequelize;
