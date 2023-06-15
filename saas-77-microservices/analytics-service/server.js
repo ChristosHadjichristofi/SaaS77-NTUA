6ef8aaeceb626b343e42a2f2009184b708ecff41
Christos Hadjichristofi <christoshadjichristofi@hotmail.com>
2023-06-15 12:50:14 +0300

    Revert "Add functionality create schema if not exists"
    This reverts commit 24186391df3af5556beaaf9cd255dc5d868ac51b.

--- saas-77-microservices/analytics-service/server.js
+++ saas-77-microservices/analytics-service/server.js
@@ -9,94 +9,69 @@ const insertEvents = require('./utils/insertEvents');
 
 // for database
 const sequelize = require('./utils/database');
-const initModels = require("./models/init-models");
-const models = initModels(sequelize);
+var initModels = require("./models/init-models");
+var models = initModels(sequelize);
 
 const port = Number(4004);
 
-const createSchema = async () => {
-  try {
-    // Connect to the database
-    await sequelize.authenticate();
-
-    // Create the schema (database)
-    await sequelize.query(`CREATE SCHEMA IF NOT EXISTS ${ process.env.DB_SCHEMA }`);
-
-    console.log("Schema created successfully");
-  } catch (error) {
-    console.error("Error creating schema:", error);
-  } finally {
-    // Close the database connection
-    await sequelize.close();
-  }
-};
-
-const startServer = async () => {
-  try {
-    // Create the schema (database)
-    await createSchema();
-
-    // Initialize Sequelize models
-    initModels(sequelize);
-
-    // Sync models with the database
-    await sequelize.sync({
-      // delete if system is ready to deploy
-      force: true,
-      // end
-    });
-
-    app.listen(port, () => {
-      console.log(chalk.green(`🚀 Analytics Service running on port ${port}!`));
-    });
-
-    let counter;
-
-    /* retrieve how many events this specific service parsed and processed */
-    let retrieveEventsPromise = new Promise((resolve, reject) => {
-      models.Events.findAll({ raw: true, where: { id: 1 } })
-        .then((rows) => {
-          /* if no rows returned then processed 0 events, create the row with id = 1 and counter = 0 */
-          if (rows.length == 0) models.Events.create({ counter: 0 });
-          else counter = rows[0].counter;
-
-          resolve();
+initModels(sequelize);
+sequelize
+    .sync({
+        // delete if system is ready to deploy
+        force: true,
+        // end
+    })
+    .then((result) => {
+        
+        app.listen(port, () => {
+            console.log(chalk.green(`🚀 Analytics Service running on port ${port}!`));
         });
-    });
+        
+        let counter;
 
-    /* after retrieving the events counter make a request to the bus to learn about all the events that have id gt counter */
-    Promise.all([retrieveEventsPromise]).then(() => {
-      if (counter === undefined) counter = 0;
+        /* retrieve how many events this specific service parsed and processed */
+        let retrieveEventsPromise = new Promise((resolve, reject) => {
+            models.Events.findAll({ raw: true, where: { id : 1 } })
+            .then(rows => {
+                /* if no rows returned then processed 0 events, create the row with id = 1 and counter = 0 */
+                if (rows.length == 0) models.Events.create({ counter: 0 })
+                else counter = rows[0].counter;
 
-      /* endpoint to get lost events */
-      const url = 'http://localhost:4006/events/' + counter;
-
-      /* construct token so as the service can make req to the bus */
-      const token = jwt.sign({ service: 'Analytics Service' }, process.env.SECRET_JWT, { expiresIn: '20s' });
-
-      /* add token and secret string that services share (allowed origin) */
-      const headers = {
-        'X-OBSERVATORY-AUTH': token,
-        "CUSTOM-SERVICES-HEADER": JSON.stringify(encrypt(process.env.SECRET_STRING_SERVICES))
-      };
-
-      /* make the request and get all data */
-      const config = { method: 'get', url: url, headers: headers };
-
-      axios(config)
-        .then(result => {
-          /* if result has length != 0 then insert events to the database asynchronously */
-          if (result.data.events.length !== 0) result.data.events.forEach(el => insertEvents(el));
+                resolve();
+            })
         })
-        .catch(err => {
-          if (err.code === 'ECONNREFUSED') console.log('Event Bus is not running.');
-          else console.log(err);
-        });
-    });
-  } catch (error) {
-    console.error("Error starting server:", error);
-  }
-};
 
-// Call startServer function to create schema and start the server
-startServer();
+        /* after retrieving the events counter make a request to the bus to learn about all the events that have id gt counter */
+        Promise.all([retrieveEventsPromise]).then(() => {
+
+            if (counter === undefined) counter = 0;
+
+            /* endpoint to get lost events */
+            const url = 'http://localhost:4006/events/' + counter;
+
+            /* construct token so as the service can make req to the bus */
+            const token = jwt.sign({ service: 'Analytics Service' }, process.env.SECRET_JWT, { expiresIn: '20s' });
+
+            /* add token and secret string that services share (allowed origin) */
+            const headers = {
+                'X-OBSERVATORY-AUTH': token,
+                "CUSTOM-SERVICES-HEADER": JSON.stringify(encrypt(process.env.SECRET_STRING_SERVICES))
+            };
+            
+            /* make the request and get all data */
+            const config = { method: 'get', url: url, headers: headers };
+            
+            axios(config)
+            .then(result => {
+                /* if result has length != 0 then insert events to the database asynchronously */
+                if (result.data.events.length !== 0) result.data.events.forEach(el => insertEvents(el));
+            })
+            .catch(err => { 
+                if (err.code === 'ECONNREFUSED') console.log('Event Bus is not running.');
+                else console.log(err);
+            })
+
+        })
+
+    })
+    .catch((err) => console.log(err));
